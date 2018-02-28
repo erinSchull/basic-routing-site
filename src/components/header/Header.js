2bba6e974a76be7448e51bd9f05f1e316c10b524
Erin Schull <erin.schull@gmail.com>
2018-02-27 22:01:19 -0700

    header links on images

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,24 +1,28 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
+import routerIcon from './../../Images/router.ico';
+import sassIcon from './../../Images/sass-white.png';
+import toDoIcon from './../../Images/list-icon.png';
+import homeIcon from './../../Images/home-icon.png';
 import icon from './../../Images/code-icon.ico';
 
 class Header extends Component {
     render(){
         return (
             <header className='header' >
-                <img src={icon} alt= 'spinning icon' />
+                <img src={icon} alt= 'code icon' />
                 <Link className = 'links' to='/' >
-                <p>Home</p>
+                <img src={homeIcon} alt='Home' className='home-icon' />
                 </Link>
                 <Link className = 'links' to='/instructions' >
-                <p>Router Instructions</p>
+                <img src={routerIcon} alt='Router Instructions' className='router-icon' />
                 </Link>
                 <Link className = 'links' to='/sass' >
-                <p>All about that Sass</p>
+                <img src={sassIcon} alt='All About That Sass' className='sass-icon' />
                 </Link>
                 <Link className = 'links' to='/to-do' >
-                <p>To Do List</p>
+                <img src={toDoIcon} alt='To Do List' className='list-icon' />
                 </Link>
             </header>
         )
--- src/components/sassPage/SassPage.js
+++ src/components/sassPage/SassPage.js
@@ -1,30 +1,33 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class SassPage extends Component {
     render() {
         return (
-            <div>
-            <h1>Let's discuss Sass</h1>
-
-            <p>Exciting, no?</p>
-            <p>Girl, put some dang stuff here!</p>
-            <p>See what I did there? ;)</p>
-
-            <h3>Let's get down to business</h3>
-            <h4>To discuss</h4>
-            <h4>The Sass...</h4>
-            <break />
-            <h3>Step 1:</h3>
-            <p>Install appropriate dependencies:</p>
-            <ul>
-                <li>node-sass-chokidar (no, do not need node otherwise!)</li>
-                <li>npm-run-all</li>
-            </ul>
-            <h3>Step 2:</h3>
-            <p>Set up root variables in _variables.scss in src folder</p>
-            <break />
-            <h3>Step 3:</h3>
-            <p>Use variables in sass files, that have matching names as the components (case sensitive)</p>
+            <div id="sass-page" >
+                <h1 id="sass-title" >Let's discuss Sass</h1>
+                <h3 className='biz' >Let's get down to business</h3>
+                <h4 className='discuss' >To discuss</h4>
+                <h4 className='sass-sub' >The Sass...</h4>
+                <break />
+                <h3 className='step-title' >Step 1:</h3>
+                <p>Install appropriate dependencies:</p>
+                <ul>
+                    <li>node-sass-chokidar (no, do not need node otherwise!)</li>
+                    <li>npm-run-all</li>
+                </ul>
+                <h3 className='step-title' >Step 2:</h3>
+                <p>Set up root variables in _variables.scss in src folder</p>
+                <break />
+                <h3 className='step-title' >Step 3:</h3>
+                <p>Use variables in sass files, that have matching names as the components (case sensitive)</p>
+                <h3 className='step-title' >Step 4:</h3>
+                    <strong>Important!</strong> <br />
+                <p>
+                To get the sass that you have now implemented, we'll need to delete the line
+                    <br />
+                    <code> import './app.css'</code> <br />
+                     and replace it with <br /> <code>import './main.css' </code> <br /> of course minding the file path (though the given is best practice) 
+                </p>
             </div>
         )
     }
